fb8efb23727e6b9a1b808e84394d974f278c8205
some body <featherboxen@gmail.com>
2020-02-26 03:29:06 -0500

    still trying to figure out autosizing on chrome

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let pages = document.getElementById("pages");
+let notif = document.getElementById("notif");
 
 const create_page = (tit,cont) => {
     const page = document.createElement("div");
@@ -42,7 +43,63 @@ const create_page = (tit,cont) => {
     page.appendChild(head);
     page.appendChild(bod);
 
-    pages.appendChild(page)
+    pages.appendChild(page);
 }
+
+/*
+<div class="page">
+    <div class="head">
+        <div class="title nodot">set name</div>
+        <div class="buttons">
+        <div class="con">c</div><div class="del">x</div>
+        </div>
+    </div>
+    <div class="body" contenteditable="true">
+        an Old World bird related to the starlings that feeds on fruit and insects, the male typically having bright yellow and black plumage.
+    </div>
+</div>
+*/
+
+const set_name = (page_num) => {
+    
+
+    const page = document.createElement("div");
+    page.className = "page";
+
+    const head = document.createElement("div");
+    head.className = "head";
+
+    const title = document.createElement("div");
+    title.className = "title";
+    title.innerText = "Set Name";
+    head.appendChild(title);
+
+    const buttons = document.createElement("div");
+    buttons.className = "buttons";
+    const btn_confirm = document.createElement("div");
+    btn_confirm.className = "con";
+    btn_confirm.innerText = "o";
+    const btn_close = document.createElement("div");
+    btn_close.className = "del";
+    btn_close.innerText = "x";
+
+    buttons.appendChild(btn_confirm);
+    buttons.appendChild(btn_close);
+
+    head.appendChild(buttons);
+
+    const bod = document.createElement("div");
+    bod.className = "body";
+    bod.innerText = "Unnamed";
+    bod.contentEditable = true;
+
+    page.appendChild(head);
+    page.appendChild(bod);
+
+    notif.appendChild(page);
+    notif.appendChild(page);
+}
+
+set_name(-1)
 
 create_page("Oriole","an Old World bird related to the starlings that feeds on fruit and insects, the male typically having bright yellow and black plumage.");
