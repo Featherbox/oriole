3aba6a27bf78979e011bc43c54eb29713b9c1f94
some body <featherboxen@gmail.com>
2020-02-26 15:22:49 -0500

    rename,minimize and delete fully implemented

--- script.js
+++ script.js
@@ -18,15 +18,29 @@ const create_page = (tit,cont) => {
     const btn_rename = document.createElement("div");
     btn_rename.className = "ren";
     btn_rename.innerText = "r";
+    let i = document.getElementsByClassName("page").length;
+    btn_rename.onclick = (e) => {
+        set_name(i);
+    }
     const btn_save = document.createElement("div");
     btn_save.className = "sav";
     btn_save.innerText = "s";
     const btn_min = document.createElement("div");
     btn_min.className = "min";
     btn_min.innerText = "-";
+    btn_min.onclick = (e) => {
+        if (page.classList.toggle("minimize")) {
+            btn_min.innerText = "+";
+        } else {
+            btn_min.innerText = "-";
+        }
+    }
     const btn_delete = document.createElement("div");
     btn_delete.className = "del";
     btn_delete.innerText = "x";
+    btn_delete.onclick = (e) => {
+        page.parentNode.removeChild(page);
+    }
 
     buttons.appendChild(btn_rename);
     buttons.appendChild(btn_save);
@@ -64,12 +78,14 @@ const set_name = (page_num) => {
     let n = "set name";
     let fn = (n) => {
         let ps = document.getElementsByClassName("page");
-        ps[page_num].getElementsByClassName("title").innerHTML = n
+        
+        console.log(`${page_num} : ${ps[page_num].getElementsByClassName("title").innerHTML}`);
+        ps[page_num].getElementsByClassName("title")[0].innerHTML = n;
     };
 
     if (page_num < 0) {
         n = "create file";
-        fn = (n) => create_page(n,"");
+        fn = (n) => create_page(n.trim(),"");
     }
 
     const page = document.createElement("div");
@@ -88,6 +104,11 @@ const set_name = (page_num) => {
     const btn_confirm = document.createElement("div");
     btn_confirm.className = "con";
     btn_confirm.innerText = "o";
+    btn_confirm.onclick = (e) => {
+        fn(bod.value);
+        while (notif.lastChild) notif.removeChild(notif.lastChild)
+        notif.style.display = "none";
+    };
     const btn_close = document.createElement("div");
     btn_close.className = "del";
     btn_close.innerText = "x";
@@ -97,13 +118,22 @@ const set_name = (page_num) => {
 
     head.appendChild(buttons);
 
-    const bod = document.createElement("div");
+    const f = document.createElement("form");
+    const bod = document.createElement("input");
     bod.className = "body";
-    bod.innerText = "Unnamed";
-    bod.contentEditable = true;
+    bod.type = "text";
+    bod.placeholder = "Unnamed";
+    
+    f.onsubmit = (e) => {
+        fn(bod.value);
+        while (notif.lastChild) notif.removeChild(notif.lastChild)
+        notif.style.display = "none";
+    };
+
+    f.appendChild(bod);
 
     page.appendChild(head);
-    page.appendChild(bod);
+    page.appendChild(f);
 
     while (notif.lastChild) notif.removeChild(notif.lastChild)
     notif.appendChild(page);
@@ -111,4 +141,4 @@ const set_name = (page_num) => {
     notif.style.display = "block";
 }
 
-create_page("Oriole","<em><strong>noun</strong> - an Old World bird related to the starlings that feeds on fruit and insects, the male typically having bright yellow and black plumage.</em>");
+create_page("Oriole","<em><strong>noun</strong> - an Old World bird related to the starlings that feeds on fruit and insects, the male typically having bright yellow and black plumage.</em><br/><br/>Trouble");
